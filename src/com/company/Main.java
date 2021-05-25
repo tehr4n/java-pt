7c63f0a770262a157cb20b21fc90925409b74f1c
unknown <sina.for.sec@gmail.com>
2021-05-25 09:27:27 +0430

    aaa

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,7 +7,12 @@ public class Main {
     public static void main(String[] args) {
 
         String malicious_data = "../../../../../etc/passwd";
+        String traversal_string = "../";
+        String traversal_string2 = "../";
         malicious_data = malicious_data.replace("../", "");
+        malicious_data = malicious_data.replace("./", "");
+        malicious_data = malicious_data.replace(traversal_string, "");
+        malicious_data = malicious_data.replace(traversal_string2, "");
 
         System.out.println(malicious_data);
 
